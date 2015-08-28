779b565d9227b95d7f94ba9c2948a183e7f4a10c
Jens Alm <jens.alm@prorenata.se>
2015-08-28 12:02:26 +0200

    script correction

--- django_castle/templatetags/castle.py
+++ django_castle/templatetags/castle.py
@@ -17,7 +17,7 @@ def castle_load(user=None, secure=False, track=False, ):
 	script = """
 <script type="text/javascript">
 	(function(e,t,n,r){function i(e,n){e=t.createElement("script");e.async=1;e.src=r;n=t.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)}e[n]=e[n]||function(){(e[n].q=e[n].q||[]).push(arguments)};e.attachEvent?e.attachEvent("onload",i):e.addEventListener("load",i,false)})(window,document,"_castle","//d2t77mnxyo7adj.cloudfront.net/v1/c.js")
-	_castle('setAppId', 'YOUR_CASTLE_APP_ID');	_castle('setAppId', '%(app_id)s');
+	_castle('setAppId', '%(app_id)s');
 """ % {"app_id": app_id}
 
 	if user:
