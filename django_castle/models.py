8c7daaa1ba972df6fe6be00b4d5d7d443acdc634
Max Leblanc <mleblanc@poka.io>
2016-06-10 11:59:23 -0400

    Added login-fail support

--- django_castle/models.py
+++ django_castle/models.py
@@ -7,14 +7,21 @@ __author__ = 'jens'
 
 
 def catch_login_signal(sender, request, user, **kwargs):
-	castle = Castle()
-	castle.log_login_success(user, request)
+    castle = Castle()
+    castle.log_login_success(user, request)
 
 auth.signals.user_logged_in.connect(catch_login_signal, dispatch_uid="castle_login_signal")
 
 
 def catch_logout_signal(sender, request, user, **kwargs):
-	castle = Castle()
-	castle.log_logout_success(user, request)
+    castle = Castle()
+    castle.log_logout_success(user, request)
 
 auth.signals.user_logged_out.connect(catch_logout_signal, dispatch_uid="castle_logout_signal")
+
+
+def catch_loginfail_signal(sender, request, user, **kwargs):
+    castle = Castle()
+    castle.log_login_fail(user, request)
+
+auth.signals.user_login_failed.connect(catch_loginfail_signal, dispatch_uid="castle_loginfail_signal")
