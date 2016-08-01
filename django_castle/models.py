d25baa22c54e641745307e5694dd07e2db163c88
Max Leblanc <mleblanc@poka.io>
2016-08-01 16:53:31 -0400

    Modified login and logout handling for request.user

--- django_castle/models.py
+++ django_castle/models.py
@@ -7,15 +7,19 @@ __author__ = 'jens'
 # Here we mainly define the signals to integrate into castle.io
 
 
-def catch_login_signal(sender, request, enable_logging=False, **kwargs):
+def catch_login_signal(sender, request, user=None, enable_logging=False, **kwargs):
     castle = Castle()
+    if not request.user:
+        request.user = user
     castle.log_event(request, castle.LOGIN_SUCCESS, enable_logging=enable_logging)
 
 auth.signals.user_logged_in.connect(catch_login_signal, dispatch_uid="castle_login_signal")
 
 
-def catch_logout_signal(sender, request, enable_logging=False, **kwargs):
+def catch_logout_signal(sender, request, user=None, enable_logging=False, **kwargs):
     castle = Castle()
+    if not request.user:
+        request.user = user
     castle.log_event(request, castle.LOGOUT_SUCCEEDED, enable_logging=enable_logging)
 
 auth.signals.user_logged_out.connect(catch_logout_signal, dispatch_uid="castle_logout_signal")
