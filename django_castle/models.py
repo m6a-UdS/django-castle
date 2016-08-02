5ba1bef1e2bdfca0ac7f048d99f4f03e9a3639fd
Max Leblanc <mleblanc@poka.io>
2016-08-02 14:49:12 -0400

    Handle two kinds of login signals

--- django_castle/models.py
+++ django_castle/models.py
@@ -7,9 +7,12 @@ __author__ = 'jens'
 # Here we mainly define the signals to integrate into castle.io
 
 
-def catch_login_signal(sender, request, enable_logging=False, **kwargs):
+def catch_login_signal(sender, request, user, enable_logging=False, **kwargs):
     castle = Castle()
-    castle.log_event(request, castle.LOGIN_SUCCESS, enable_logging=enable_logging)
+    if request.user:
+        castle.log_event(request, castle.LOGIN_SUCCESS, enable_logging=enable_logging)
+    else:
+        castle.log_login_success(user, request)
 
 auth.signals.user_logged_in.connect(catch_login_signal, dispatch_uid="castle_login_signal")
 
